6fced869efc4d7e53287f681ae23c8995a5b60c9
m09 <142691+m09@users.noreply.github.com>
2022-12-25 02:23:01 +0100

    Fix gradient descent
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- manims/gradient_descent.py
+++ manims/gradient_descent.py
@@ -1,32 +1,28 @@
 from pathlib import Path
-from typing import Tuple
+from typing import Sequence, Tuple
 
 from manim import (
     BLUE,
-    DEGREES,
     DOWN,
     GOLD,
     GREEN,
-    GREEN_D,
-    GREEN_E,
     LEFT,
     PURPLE,
     RED,
     RIGHT,
     UP,
-    DashedLine,
+    ArrowVectorField,
+    Axes,
+    DecimalNumber,
     Dot,
     Line,
-    MathTex,
     NumberPlane,
-    Surface,
+    Scene,
     Tex,
-    ThreeDAxes,
-    ThreeDScene,
-    Transform,
+    ValueTracker,
     VGroup,
 )
-from numpy import array, load, ndarray, stack, zeros_like
+from numpy import load, ndarray, stack, zeros_like
 
 data_path = Path(__file__).parent.parent / "data" / "ames.npz"
 
@@ -34,14 +30,9 @@ arrays = load(data_path)
 xs, ys = arrays["x"], arrays["y"]
 coords = stack((xs, ys, zeros_like(xs)), axis=1)
 
-XLIM_2D = (-2.5, 2.5)
-YLIM_2D = (-2.5, 2.5)
-
-XLIM_3D = (-3, 3)
-YLIM_3D = (-3, 3)
-SHIFT_LEFT = LEFT * 4.5 + UP * 1.5
-ALIGN_LEFT = LEFT * 7
-SHIFT_RIGHT = RIGHT * 6 + DOWN * 3
+XLIM = (-3, 3)
+YLIM = (-3, 3)
+SCALE_FORMULAS = 0.7
 
 
 def mse(theta_0: float, theta_1: float) -> float:
@@ -68,172 +59,195 @@ def update(theta_0: float, theta_1: float, lr: float) -> Tuple[float, float]:
     return theta_0 - lr * g_0, theta_1 - lr * g_1
 
 
-def init() -> Tuple[float, float]:
-    return -0.98, -0.76
-
-
-def get_line(theta_0: float, theta_1: float) -> Line:
-    return Line(
-        array([XLIM_2D[0], theta_0 + theta_1 * XLIM_2D[0], 0]) + SHIFT_LEFT,
-        array([XLIM_2D[1], theta_0 + theta_1 * XLIM_2D[1], 0]) + SHIFT_LEFT,
-        color=RED,
-    )
-
-
-def get_predictions(theta_0: float, theta_1: float) -> VGroup:
-    return VGroup(
-        *(
-            Dot(array([x, theta_0 + theta_1 * x, 0]) + SHIFT_LEFT, color=RED, z_index=1)
-            for x in xs
-        )
-    )
-
-
 def get_links(dots: VGroup, predictions: VGroup) -> VGroup:
     return VGroup(*(Line(d, p, color=GREEN) for d, p in zip(dots, predictions)))
 
 
-def get_y_formula(theta_0: float, theta_1: float) -> MathTex:
-    theta_1_tex = f"{theta_1:.2f}"
-    theta_0_tex = f"- {-theta_0:.2f}" if theta_0 < 0 else f"+ {theta_0:.2f}"
-    tex = (
-        MathTex(r"\hat{y} = ", theta_1_tex, " x ", theta_0_tex, color=RED)
-        .scale(0.75)
-        .align_to(ALIGN_LEFT, LEFT)
-        .shift(DOWN * 3)
-    )
-    tex[1].set_color(GOLD)
-    tex[3].set_color(PURPLE)
-    return tex
-
-
-def get_loss_formula(loss: float) -> Tex:
-    return (
-        Tex(rf"$E = {loss:.2f}$", color=GREEN)
-        .scale(0.75)
-        .align_to(ALIGN_LEFT, LEFT)
-        .shift(DOWN * 1.8)
-    )
-
-
-def get_theta_0_formula(theta_0: float) -> Tex:
-    return (
-        Tex(f"$b = {theta_0:.2f}$", color=PURPLE)
-        .scale(0.75)
-        .align_to(ALIGN_LEFT, LEFT)
-        .shift(DOWN * 2.2)
-    )
-
-
-def get_theta_1_formula(theta_1: float) -> Tex:
-    return (
-        Tex(f"$a = {theta_1:.2f}$", color=GOLD)
-        .scale(0.75)
-        .align_to(ALIGN_LEFT, LEFT)
-        .shift(DOWN * 2.6)
-    )
-
-
-class ErrorSurface(ThreeDScene):
-    def construct(self) -> None:
-        axes = ThreeDAxes(
-            x_range=(*XLIM_3D, 1),
-            y_range=(*YLIM_3D, 1),
-            z_range=(0, 6, 1),
+class ErrorVectorField(VGroup):
+    def __init__(self, theta_0: ValueTracker, theta_1: ValueTracker) -> None:
+        super().__init__()
+        ax = Axes(
+            x_range=(*XLIM, 1),
+            y_range=(*YLIM, 1),
+            x_length=sum(XLIM),
+            y_length=sum(YLIM),
             x_axis_config=dict(color=PURPLE),
             y_axis_config=dict(color=GOLD),
-            z_axis_config=dict(color=GREEN),
         )
-        axes.shift(SHIFT_RIGHT)
-        self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
-        error_surface = Surface(
-            lambda u, v: array(
-                [
-                    u,
-                    v,
-                    mse(u, v),
-                ]
-            ),
-            u_range=XLIM_3D,
-            v_range=YLIM_3D,
-            checkerboard_colors=(GREEN_D, GREEN_E),
+        theta_0_label = Tex(r"$\theta_0$", color=PURPLE).next_to(ax, RIGHT)
+        theta_1_label = Tex(r"$\theta_1$", color=GOLD).next_to(ax, UP)
+        title = Tex(r"Erreur en fonction des $\theta$").next_to(theta_1_label, UP)
+        vector_field = ArrowVectorField(
+            func=lambda p: tuple(-x for x in gradient(p[0], p[1])),
+            x_range=list(XLIM),
+            y_range=list(YLIM),
+            opacity=0.5,
         )
-        error_surface.set_opacity(0.3)
-        error_surface.shift(SHIFT_RIGHT)
-        self.add(axes, error_surface)
-
-        number_plane = NumberPlane(x_range=(*XLIM_2D, 1), y_range=(*YLIM_2D, 1))
-        self.dots = VGroup(*(Dot(c, color=BLUE) for c in coords))
-        number_plane.add(self.dots)
-        number_plane.shift(SHIFT_LEFT)
-        self.add_fixed_in_frame_mobjects(number_plane)
-
-        theta_0, theta_1 = 0.0, 0.0
-        loss = mse(theta_0, theta_1)
-        self.y_formula = get_y_formula(theta_0, theta_1)
-        self.theta_0_formula = get_theta_0_formula(theta_0)
-        self.theta_1_formula = get_theta_1_formula(theta_1)
-        self.loss_formula = get_loss_formula(loss)
-        self.params_dot = Dot([theta_0, theta_1, 0], color=RED)
-        self.loss_dot = Dot([theta_0, theta_1, loss], color=RED)
-        self.loss_line = DashedLine(
-            self.params_dot,
-            self.loss_dot,
-            color=RED,
+        params = Dot(
+            ax.c2p(theta_0.get_value(), theta_1.get_value(), 0), color=GREEN, z_index=0
         )
-        self.loss_dot.shift(SHIFT_RIGHT)
-        self.params_dot.shift(SHIFT_RIGHT)
-        self.loss_line.shift(SHIFT_RIGHT)
-        self.line = get_line(theta_0, theta_1)
-        self.predictions = get_predictions(theta_0, theta_1)
-        self.links = get_links(self.dots, self.predictions)
-        self.add(self.loss_dot, self.loss_line, self.params_dot)
-        self.add_fixed_in_frame_mobjects(
-            self.line,
-            self.predictions,
-            self.links,
-            self.y_formula,
-            self.theta_0_formula,
-            self.theta_1_formula,
-            self.loss_formula,
+        params.add_updater(lambda p: p.set_x(ax.c2p(theta_0.get_value(), 0, 0)[0]))
+        params.add_updater(lambda p: p.set_y(ax.c2p(0, theta_1.get_value(), 0)[1]))
+        self.add(
+            ax,
+            theta_0_label,
+            theta_1_label,
+            title,
+            vector_field,
+            params,
         )
 
-        self._update_from_thetas(2, theta_1)
-        self._update_from_thetas(-2, theta_1)
-        self._update_from_thetas(theta_0, theta_1)
-        self._update_from_thetas(theta_0, 2)
-        self._update_from_thetas(theta_0, -2)
-        self._update_from_thetas(theta_0, theta_1)
 
-        theta_0, theta_1 = init()
-        self._update_from_thetas(theta_0, theta_1)
+class Formulas(VGroup):
+    def __init__(self, theta_0: ValueTracker, theta_1: ValueTracker) -> None:
+        super().__init__()
+        self._theta_0 = theta_0
+        self._theta_1 = theta_1
+
+        error_formula = self._create_error_vgroup()
+        theta_0_formula = self._create_theta_0_vgroup()
+        theta_1_formula = self._create_theta_1_vgroup()
+        y_formula = self._create_y_vgroup()
+
+        theta_0_formula.next_to(error_formula, DOWN).align_to(error_formula, LEFT)
+        theta_1_formula.next_to(theta_0_formula, DOWN).align_to(error_formula, LEFT)
+        y_formula.next_to(theta_1_formula, DOWN).align_to(error_formula, LEFT)
+
+        self.add(error_formula, theta_0_formula, theta_1_formula, y_formula)
+
+    def _create_y_vgroup(self) -> VGroup:
+        label = Tex(r"$\hat{y}$ = ", color=RED)
+
+        theta_1 = DecimalNumber(self._theta_1.get_value(), color=GOLD)
+        theta_1.add_updater(lambda x: x.set_value(self._theta_1.get_value()))
+
+        between = Tex("$x$", color=RED)
+        between.add_updater(lambda x: x.next_to(theta_1, buff=0.1))
+
+        theta_0 = DecimalNumber(
+            self._theta_0.get_value(), color=PURPLE, include_sign=True
+        )
+        theta_0.add_updater(lambda x: x.set_value(self._theta_0.get_value()))
+        theta_0.add_updater(lambda x: x.next_to(between, RIGHT))
+
+        theta_1.next_to(label, RIGHT)
+        between.next_to(theta_1, RIGHT, buff=0.1)
+        theta_0.next_to(between, RIGHT)
+
+        return VGroup(label, theta_1, between, theta_0)
+
+    def _create_error_vgroup(self) -> VGroup:
+        label = Tex("E = ", color=GREEN)
+        value = DecimalNumber(
+            mse(self._theta_0.get_value(), self._theta_1.get_value()), color=GREEN
+        )
+        value.add_updater(
+            lambda x: x.set_value(
+                mse(self._theta_0.get_value(), self._theta_1.get_value())
+            )
+        )
+        value.next_to(label, RIGHT)
+        return VGroup(label, value)
+
+    def _create_theta_vgroup(
+        self, tracker: ValueTracker, label: str, color: str
+    ) -> VGroup:
+        label = Tex(label, color=color)
+        value = DecimalNumber(tracker.get_value(), color=color)
+        value.add_updater(lambda x: x.set_value(tracker.get_value()))
+        value.next_to(label, RIGHT)
+        return VGroup(label, value)
+
+    def _create_theta_0_vgroup(self) -> VGroup:
+        return self._create_theta_vgroup(self._theta_0, r"$\theta_0$ = ", PURPLE)
+
+    def _create_theta_1_vgroup(self) -> VGroup:
+        return self._create_theta_vgroup(self._theta_1, r"$\theta_1$ = ", GOLD)
+
+
+class Data(VGroup):
+    def __init__(self, theta_0: ValueTracker, theta_1: ValueTracker) -> None:
+        super().__init__()
+        self._theta_0 = theta_0
+        self._theta_1 = theta_1
+        number_plane = NumberPlane(x_range=(*XLIM, 1), y_range=(*YLIM, 1))
+        dots = VGroup(*(Dot(c, color=BLUE) for c in coords))
+        number_plane.add(dots)
+        title = Tex("Données").next_to(number_plane, UP).shift(UP * 0.75)
+        line = Line(color=RED).add_updater(
+            lambda line: line.put_start_and_end_on(
+                number_plane.c2p(XLIM[0], self._predict(XLIM[0]), 0),
+                number_plane.c2p(XLIM[1], self._predict(XLIM[1]), 0),
+            )
+        )
+        preds = VGroup(
+            *(
+                Dot(
+                    number_plane.c2p(c[0], self._predict(c[1]), 0), color=RED
+                ).add_updater(
+                    lambda d: d.set_y(
+                        number_plane.c2p(
+                            0, self._predict(number_plane.p2c(d.get_center())[0]), 0
+                        )[1]
+                    )
+                )
+                for c in coords
+            )
+        )
+
+        def _create_line(dot: Dot, pred: Dot) -> Line:
+            return Line(dot, pred, color=GREEN).add_updater(
+                lambda line: line.put_start_and_end_on(
+                    dot.get_center(),
+                    number_plane.c2p(
+                        number_plane.p2c(dot.get_center())[0],
+                        self._predict(number_plane.p2c(dot.get_center())[0]),
+                    ),
+                )
+            )
+
+        errors = VGroup(*(_create_line(dot, pred) for dot, pred in zip(dots, preds)))
+        self.add(number_plane, title, line, preds, errors)
+
+    def _predict(self, x: float) -> float:
+        return self._theta_0.get_value() + self._theta_1.get_value() * x
+
+
+class ErrorSurface(Scene):
+    def construct(self) -> None:
+
+        theta_0, theta_1 = ValueTracker(0.0), ValueTracker(0.0)
+
+        error_vgroup = ErrorVectorField(theta_0, theta_1)
+        error_vgroup.shift(RIGHT * 4).scale(SCALE_FORMULAS)
+
+        formulas_vgroup = Formulas(theta_0, theta_1)
+        formulas_vgroup.shift(UP + LEFT * 1.8).scale(SCALE_FORMULAS)
+
+        data_vgroup = Data(theta_0, theta_1)
+        data_vgroup.shift(LEFT * 4.5).scale(SCALE_FORMULAS)
+
+        self.add(data_vgroup, formulas_vgroup, error_vgroup)
+
+        self.play(theta_0.animate.set_value(2))
+        self.play(theta_0.animate.set_value(-2))
+        self.play(theta_0.animate.set_value(0))
+
         self.wait(1)
-        for n in range(15):
-            self.wait(1)
-            theta_0, theta_1 = update(theta_0, theta_1, 0.2)
-            self._update_from_thetas(theta_0, theta_1)
-        self.wait(4)
+        self.play(theta_1.animate.set_value(2))
+        self.play(theta_1.animate.set_value(-2))
+        self.play(theta_1.animate.set_value(0))
 
-    def _update_from_thetas(self, theta_0: float, theta_1: float) -> None:
-        loss = mse(theta_0, theta_1)
-        new_loss_dot = Dot(
-            array([theta_0, theta_1, loss]) + SHIFT_RIGHT,
-            color=RED,
-        )
-        new_params_dot = Dot(array([theta_0, theta_1, 0]) + SHIFT_RIGHT, color=RED)
-        new_predictions = get_predictions(theta_0, theta_1)
-        self.play(
-            Transform(self.params_dot, new_params_dot),
-            Transform(self.loss_dot, new_loss_dot),
-            Transform(
-                self.loss_line,
-                DashedLine(new_params_dot, new_loss_dot, color=RED),
-            ),
-            Transform(self.line, get_line(theta_0, theta_1)),
-            Transform(self.predictions, new_predictions),
-            Transform(self.links, get_links(self.dots, new_predictions)),
-            Transform(self.y_formula, get_y_formula(theta_0, theta_1)),
-            Transform(self.theta_0_formula, get_theta_0_formula(theta_0)),
-            Transform(self.theta_1_formula, get_theta_1_formula(theta_1)),
-            Transform(self.loss_formula, get_loss_formula(loss)),
-        )
+        self.wait(1)
+        self.play(theta_0.animate.set_value(-0.98), theta_1.animate.set_value(-0.76))
+
+        for _ in range(15):
+            self.wait(1)
+            new_theta_0, new_theta_1 = update(
+                theta_0.get_value(), theta_1.get_value(), 0.2
+            )
+            self.play(
+                theta_0.animate.set_value(new_theta_0),
+                theta_1.animate.set_value(new_theta_1),
+            )
+        self.wait(4)
